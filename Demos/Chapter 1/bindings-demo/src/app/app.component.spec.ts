ed8f93c698981a303a66896fa07ff42ca77585d4
Kevin Rattan <rattan.kevin.k@gmail.com>
2024-09-18 16:22:48 -0400

    To end ex 1.2 in 2.1 start point

--- Demos/Chapter 1/bindings-demo/src/app/app.component.spec.ts
+++ Demos/Chapter 1/bindings-demo/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bind'`, () => {
+  it(`should have the 'bindings-demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bind');
+    expect(app.title).toEqual('bindings-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bind app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bindings-demo');
   });
 });
--- Demos/Chapter 1/bindings-demo/src/app/app.component.ts
+++ Demos/Chapter 1/bindings-demo/src/app/app.component.ts
@@ -1,21 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   actionName = 'Add';
   country: any = { name: 'England' };
   countries: any[] = [{ name: 'Wales' }];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   submit(): void {
     this.countries.push( { name: this.country.name });
   }
+
 }
